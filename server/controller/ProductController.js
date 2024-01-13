48453a8e7febc16a68e64a4b19170991b0776474
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-01-13 15:56:02 +0530

    backend CRUD done

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -12,6 +12,64 @@ const productController = {
             res.status(500).json({ error: "Something went wrong" });
         }
     },
+
+    // find (get one)  
+    find: async function (req, res, next) {
+        try {
+            const id = req.params.id;
+            const product = await Product.find({ id: id });
+            res.status(200).json(product);
+        } catch (error) {
+            console.log("Error ", error);
+            res.status(500).json({ error: "Something went wrong" });
+        }
+    },
+
+    // get all  
+    getAll: async function (req, res, next) {
+        try {
+            const findAll = await Product.find();
+            res.status(200).json(findAll);
+        } catch (error) {
+            console.log("Error ", error);
+            res.status(500).json({ error: "Something went wrong" });
+        }
+    },
+
+    // update
+    update: async function (req, res, next) {
+        try {
+            const id = req.params.id;
+            const productData = req.body;
+
+            const updatedProduct = await Product.findOneAndUpdate({ id: id }, productData, { new: true }); // updated product return true 
+            if (!updatedProduct) {
+                return res.status(404).json({ error: 'not found' });
+            }
+            res.status(200).json(updatedProduct);
+
+        } catch (error) {
+            console.log("Error ", error);
+            res.status(500).json({ error: "Something went wrong" });
+        }
+    },
+
+    // delete
+    delete: async function (req, res, next) {
+        try {
+            const id = req.params.id;
+            const result = await Product.deleteOne({ id: id });
+
+            if (result.deletedCount == 0) {
+                return res.status(404).json({ error: 'not found' });
+            }
+            res.status(200).json({ message: 'deleted success' });
+
+        } catch (error) {
+            console.log("Error ", error);
+            res.status(500).json({ error: "Something went wrong" });
+        }
+    }
 }
 
 module.exports = productController;
--- server/routes/product.js
+++ server/routes/product.js
@@ -5,4 +5,16 @@ const productController = require('../controller/ProductController');
 // add
 router.post('/add', productController.add);
 
+// get one
+router.get('/find/:id', productController.find);
+
+// get all 
+router.get('/all', productController.getAll);
+
+// update
+router.put('/update/:id', productController.update);
+
+// delete
+router.delete('/delete/:id', productController.delete);
+
 module.exports = router;
